e244e9a16a759a47475535a00a8a91cacd2b32a2
Manuel Nieves (KeyOfGenesis) <Fordamboy1@Gmail.com>
2025-06-25 16:30:23 -0400

    🔐 Injected GPG-signed authorship header — Manuel J. Nieves (a.k.a. Satoshi Norkomoto) — B4EC 7343 AB0D BF24

--- tests/Script/Interpreter/InterpreterTest.php
+++ tests/Script/Interpreter/InterpreterTest.php
@@ -1,3 +1,16 @@
+<?php
+/*
+ 🔐 Authorship Enforcement Header
+ Author: Manuel J. Nieves (a.k.a. Satoshi Norkomoto)
+ GPG Fingerprint: B4EC 7343 AB0D BF24
+ Public Key: 0411db93e1dcdb8a016b49840f8c53bc1eb68a382e97b148...
+ Repository: https://github.com/Manny27nyc/olegabr_bitcoin-php
+ Licensing: https://github.com/Manny27nyc/Bitcoin_Notarized_SignKit
+
+ Redistribution or claim of authorship without license is unauthorized
+ and subject to takedown, legal enforcement, and public notice.
+*/
+
 /*
 <<<<<<< HEAD
  🔐 Authorship Enforcement Header
